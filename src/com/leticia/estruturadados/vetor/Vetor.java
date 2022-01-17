ee1d2f8d9c34d78df97fe5f90562e4d6b1364dd8
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2022-01-16 23:12:35 -0300

    busca sequencial

--- src/com/leticia/estruturadados/vetor/Vetor.java
+++ src/com/leticia/estruturadados/vetor/Vetor.java
@@ -48,6 +48,15 @@ public class Vetor {
 		return this.elementos[posicao];
 	}
 	
+	public int busca(String elemento) {
+		for (int i=0; i<this.tamanho; i++) {
+			if (this.elementos[i].equals(elemento)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
 	public int tamanho() {
 		return this.tamanho;
 	}
