e3c2fe8eac0f3bba624b9cc663779112ddee8d06
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2022-01-22 19:31:18 -0300

    add descricao fila

--- src/com/leticia/estruturadados/pilha/exercicios/Exer06.java
+++ src/com/leticia/estruturadados/pilha/exercicios/Exer06.java
@@ -1,5 +1,19 @@
 package com.leticia.estruturadados.pilha.exercicios;
 
+/*
+ * Escreva um programa para verificar se uma expressão 
+ * matemática tem os paraenteses agrupados de forma correta,
+ * isto é:
+ * 
+ * (1) se o número de parêntese à esquerda e à direita são iguais e:
+ * (2) se todo parêntes aberto é seguido posteriormente por um fechamento
+ * fechamento de parêntese. 
+ *
+ * Veja alguns exemplos:
+ *- As expressões ((A+B) ou A+B (violam a condição 1
+ *- As expressões ) A+B(-C OU(A+B))-(C+D violam a condição 2
+ *- A expressão ((A+B)+D) está ok 
+ */
 public class Exer06 {
 
 	public static void main(String[] args) {
